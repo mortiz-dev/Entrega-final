836512c6915be612d8b4db55a91451bfda732b7c
Miguel <mortiz@nebuluxs.com>
2022-09-01 02:08:39 -0300

    Modificación Usuarios

---
+++ Handler/DOTS/PutUsuario.cs
@@ -0,0 +1,12 @@
+﻿namespace Entrega_final.Handler.DOTS
+{
+    public class PutUsuario
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+       
+    }
+}
--- Handler/UsuarioHandler.cs
+++ Handler/UsuarioHandler.cs
@@ -26,26 +26,14 @@ namespace MiPrimeraApi2.Repository
                             {
                                 Usuario usuario = new Usuario();
 
-                                usuario.Id = Convert.ToInt32(dataReader["Id"]);
-                                usuario.NombreUsuario = dataReader["NombreUsuario"].ToString();
-                                usuario.Nombre = dataReader["Nombre"].ToString();
-                                usuario.Apellido = dataReader["Apellido"].ToString();
-                                usuario.Password = dataReader["Contraseña"].ToString();
-                                usuario.Email = dataReader["Mail"].ToString();
-
-                                resultados.Add(usuario);
-                            }
-                        }
+                        lUsuario.Add(usuario);
                     }
-
-                    sqlConnection.Close();
                 }
             }
-
-            return resultados;
+            return lUsuario?.FirstOrDefault();
         }
-        
-        public static bool EliminarUsuario(int id)
+
+        public static List<Usuario> GetUsuarios()
         {
             bool resultado = false;
             using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
@@ -142,6 +130,81 @@ namespace MiPrimeraApi2.Repository
             }
 
             return resultado;
+        }
+        public static void CrearUsuario(Usuario user)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
+            {
+                string queryInsert = "INSERT INTO Usuario(Nombre, Apellido, NombreUsuario, Contraseña, Mail) " +
+                    "VALUES(@nombre, @apellido, @nombreUsuario, @password, @email)";
+                SqlParameter nombreParameter = new SqlParameter("nombre", SqlDbType.VarChar) { Value = user.Nombre };
+                SqlParameter apellidoParameter = new SqlParameter("apellido", SqlDbType.VarChar) { Value = user.Apellido };
+                SqlParameter nombreUsuarioParameter = new SqlParameter("nombreUsuario", SqlDbType.VarChar) { Value = user.NombreUsuario };
+                SqlParameter passwordParameter = new SqlParameter("password", SqlDbType.VarChar) { Value = user.Password };
+                SqlParameter emailParamater = new SqlParameter("email", SqlDbType.VarChar) { Value = user.Email };
+
+                sqlConnection.Open();
+
+                using (SqlCommand sqlCommand = new SqlCommand(queryInsert, sqlConnection))
+                {
+                    sqlCommand.Parameters.Add(nombreParameter);
+                    sqlCommand.Parameters.Add(apellidoParameter);
+                    sqlCommand.Parameters.Add(nombreUsuarioParameter);
+                    sqlCommand.Parameters.Add(passwordParameter);
+                    sqlCommand.Parameters.Add(emailParamater);
+
+                    sqlCommand.ExecuteNonQuery();
+                }
+                sqlConnection.Close();
+            }
+        }
+        public static void EliminarUsuario(int id)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
+            {
+                string queryDelete = "DELETE FROM Usuario WHERE Id = @id";
+
+                SqlParameter sqlParameter = new SqlParameter("id", System.Data.SqlDbType.BigInt);
+                sqlParameter.Value = id;
+
+                sqlConnection.Open();
+
+                using (SqlCommand sqlCommand = new SqlCommand(queryDelete, sqlConnection))
+                {
+                    sqlCommand.Parameters.Add(sqlParameter);
+                    sqlCommand.ExecuteNonQuery();
+                }
+                sqlConnection.Close();
+            }
+        }
+        public static void ModificarUsuario(Usuario usuario)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
+            {
+                string queryInsert = "UPDATE Usuario SET Nombre = @nombre, Apellido = @apellido, NombreUsuario = @nombreUsuario, Contraseña = @password, Mail = @email WHERE Id = @id";
+                SqlParameter idParameter = new SqlParameter("id", SqlDbType.Int) { Value = usuario.Id };
+                SqlParameter nombreParameter = new SqlParameter("nombre", SqlDbType.VarChar) { Value = usuario.Nombre };
+                SqlParameter apellidoParameter = new SqlParameter("apellido", SqlDbType.VarChar) { Value = usuario.Apellido };
+                SqlParameter nombreUsuarioParameter = new SqlParameter("nombreUsuario", SqlDbType.VarChar) { Value = usuario.NombreUsuario };
+                SqlParameter passwordParameter = new SqlParameter("password", SqlDbType.VarChar) { Value = usuario.Password };
+                SqlParameter emailParameter = new SqlParameter("email", SqlDbType.VarChar) { Value = usuario.Email };
+
+                sqlConnection.Open();
+
+                using (SqlCommand sqlCommand = new SqlCommand(queryInsert, sqlConnection))
+                {
+                    sqlCommand.Parameters.Add(idParameter);
+                    sqlCommand.Parameters.Add(nombreParameter);
+                    sqlCommand.Parameters.Add(apellidoParameter);
+                    sqlCommand.Parameters.Add(nombreUsuarioParameter);
+                    sqlCommand.Parameters.Add(passwordParameter);
+                    sqlCommand.Parameters.Add(emailParameter);
+
+                    sqlCommand.ExecuteNonQuery();
+                }
+
+                sqlConnection.Close();
+            }
         }
     }
 }
